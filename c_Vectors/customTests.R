055b508f07b2a63b960d168d07a2183612aa9be8
andrewbatran <abtran@gmail.com>
2024-10-02 11:13:46 -0400

    getstate

--- c_Vectors/customTests.R
+++ c_Vectors/customTests.R
@@ -30,6 +30,18 @@ getExpr <- function(){
   getState()$expr
 }
 
+# Get the swirl state
+getState <- function(){
+  # Whenever swirl is running, its callback is at the top of its call stack.
+  # Swirl's state, named e, is stored in the environment of the callback.
+  environment(sys.function(1))$e
+}
+
+# Retrieve the log from swirl's state
+getLog <- function(){
+  getState()$log
+}
+
 submit_log <- function(){
   
     
